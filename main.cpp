82bf8a2c8bf77d4e6b0d65055e3e5a6118270c0a
0aqz0 <18026332256@163.com>
2019-04-04 09:30:34 +0800

    fix main.cpp for combination

--- main.cpp
+++ main.cpp
@@ -55,35 +55,34 @@ int main(int argc, char *argv[])
     while(true){
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-        if(LocalPlanner::instance()->hasArrived(MyDataManager::instance()->goals.front())){
-            MyDataManager::instance()->goals.push_back(MyDataManager::instance()->goals.front());
-            MyDataManager::instance()->goals.pop_front();
-            LocalPlanner::instance()->stopMoving();
-            LocalPlanner::instance()->clearPath();
-            qDebug() << "Change Goal to " << MyDataManager::instance()->goals.front().x() << MyDataManager::instance()->goals.front().y();
-        }
-
-//        qDebug() << MyDataManager::instance()->ourRobot().x << MyDataManager::instance()->ourRobot().y;
-//        qDebug() << MyDataManager::instance()->blueRobots[4].x << MyDataManager::instance()->blueRobots[4].y;
         if_use_artifical_potential = ApPlanner::instance()->plan(MyDataManager::instance()->goals.front());
-        LocalPlanner::instance()->plan();
-//        qDebug() <<MyDataManager::instance()->yellowRobots[2].x <<MyDataManager::instance()->yellowRobots[2].y;
-        if(PARAMS::IS_SIMULATION)
-            CommandSender::instance()->sendToSim(PARAMS::our_id, LocalPlanner::instance()->velX, LocalPlanner::instance()->velY, LocalPlanner::instance()->velW);
-        else
-//            serial.sendToReal(2,100,0,0);
-//        qDebug() << MyDataManager::instance()->ourRobot().vel_x << MyDataManager::instance()->ourRobot().vel_y;
-            if (!if_use_artifical_potential)
-                serial.sendToReal(2, 30*LocalPlanner::instance()->velX, 0, -40*LocalPlanner::instance()->velW);
+        if(!if_use_artifical_potential){
+            if(LocalPlanner::instance()->hasArrived(MyDataManager::instance()->goals.front())){
+                MyDataManager::instance()->goals.push_back(MyDataManager::instance()->goals.front());
+                MyDataManager::instance()->goals.pop_front();
+                LocalPlanner::instance()->stopMoving();
+                LocalPlanner::instance()->clearPath();
+                qDebug() << "Change Goal to " << MyDataManager::instance()->goals.front().x() << MyDataManager::instance()->goals.front().y();
+            }
+
+    //        qDebug() << MyDataManager::instance()->ourRobot().x << MyDataManager::instance()->ourRobot().y;
+    //        qDebug() << MyDataManager::instance()->blueRobots[4].x << MyDataManager::instance()->blueRobots[4].y;
+            LocalPlanner::instance()->plan();
+    //        qDebug() <<MyDataManager::instance()->yellowRobots[2].x <<MyDataManager::instance()->yellowRobots[2].y;
+            if(PARAMS::IS_SIMULATION)
+                CommandSender::instance()->sendToSim(PARAMS::our_id, LocalPlanner::instance()->velX, LocalPlanner::instance()->velY, LocalPlanner::instance()->velW);
             else
-                serial.sendToReal(2,
-                                  70 * ApPlanner::instance()->v_x,
-                                  70 * ApPlanner::instance()->v_y,
-                                  -40 * ApPlanner::instance()->v_w);
-//        qDebug() << "vel: "<< LocalPlanner::instance()->velX << LocalPlanner::instance()->velW;
-//            serial.sendToReal(2, 70 * ApPlanner::instance()->v_x,
-//                             70 * ApPlanner::instance()->v_y,
-//                             -40 * ApPlanner::instance()->v_w);
+    //            serial.sendToReal(2,100,0,0);
+    //        qDebug() << MyDataManager::instance()->ourRobot().vel_x << MyDataManager::instance()->ourRobot().vel_y;
+                serial.sendToReal(2, 30*LocalPlanner::instance()->velX, 0, -40*LocalPlanner::instance()->velW);
+    //        qDebug() << "vel: "<< LocalPlanner::instance()->velX << LocalPlanner::instance()->velW;
+
+        }
+        else{
+            serial.sendToReal(2, 70 * ApPlanner::instance()->v_x,
+                              70 * ApPlanner::instance()->v_y,
+                              -40 * ApPlanner::instance()->v_w);
+        }
     }
 
     return a.exec();
